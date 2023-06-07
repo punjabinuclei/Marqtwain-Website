477b1899f37e33349dd6fcc71255a771e5953aac
punjabinuclei <guppi0204@gmail.com>
2023-06-07 22:57:31 +0530

    EmailIntergrationPartiallyDOne

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,28 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const HeroSection = () => {
-    return (
+    const [email, setEmail] = useState('');
 
-        <div className='h-screen mt-[100px] pl-[170px] pr-[201px]'>
-            <div className='text-white h-full leading-10'>
-                <div className='text-[50px] font-bold'>
-                    Crafting Products for<span className='relative top-4'><br />
-                        the Next-Gen User Experience</span>
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log('Email:', email);
+        setEmail('');
+    };
+
+    return (
+        <div className="h-screen mt-[100px] pl-[170px] pr-[201px]">
+            <div className="text-white h-full leading-10">
+                <div className="text-[50px] font-bold">
+                    Crafting Products for
+                    <span className="relative top-4">
+                        <br />
+                        the Next-Gen User Experience
+                    </span>
                 </div>
-                <div className='text-[19px] leading-7 pt-10'>
-                    Our team of experts combines research, strategy, and design to deliver<span><br />
-                        innovative solutions that meet the unique needs of your business</span>
+                <div className="text-[19px] leading-7 pt-10">
+                    Our team of experts combines research, strategy, and design to deliver
+                    <span>
+                        <br />
+                        innovative solutions that meet the unique needs of your business
+                    </span>
                 </div>
-                <div className='pt-8'>
-                    <form action="">
-                        <input type="email" style={{outline:'none'}} placeholder='Your Work Email' className='rounded-xl w-[251.66px] text-sm py-3 pl-2 hover:border-2 hover:border-Border text-black' />
-                        <button className='ml-2 px-6 py-3.5 text-sm bg-Orange rounded-xl ' >Get Started</button>
+                <div className="pt-8">
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="email"
+                            style={{ outline: 'none' }}
+                            placeholder="Your Work Email"
+                            className="rounded-xl w-[251.66px] text-sm py-3 pl-2 hover:border-2 hover:border-Border text-black"
+                            value={email}
+                            onChange={handleInputChange}
+                        />
+                        <button className="ml-2 px-6 py-3.5 text-sm bg-Orange rounded-xl">
+                            Get Started
+                        </button>
                     </form>
                 </div>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default HeroSection
+export default HeroSection;
