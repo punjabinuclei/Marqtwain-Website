f46924c173c44bfab6af62124c2ec575ecdfc525
punjabinuclei <guppi0204@gmail.com>
2023-06-26 21:00:17 +0530

    configFileUpdated

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
-module.exports = nextConfig
+const webpack = require('webpack');
+module.exports = {
+webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+config.plugins.push(new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+    }))
+return config;
+}}
