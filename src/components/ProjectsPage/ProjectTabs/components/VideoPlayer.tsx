16bdb0546dfb8efbbc6cb079deb435be8ee198ff
punjabinuclei <guppi0204@gmail.com>
2023-07-19 23:01:07 +0530

    DeploymentIssueFix

--- src/components/ProjectsPage/ProjectTabs/components/VideoPlayer.tsx
+++ src/components/ProjectsPage/ProjectTabs/components/VideoPlayer.tsx
@@ -1,35 +1,45 @@
-import React, { useRef } from 'react';
+// import React, { useRef } from 'react';
 
-interface Props {
-    src: string;
+// interface Props {
+//     src: string;
+// }
+
+
+// const VideoPlayer = ({ src }:Props) => {
+//     const videoRef = useRef(null);
+
+//     const handleMouseOver = () => {
+//         videoRef.current.play();
+//     };
+
+//     const handleMouseOut = () => {
+//         videoRef.current.pause();
+//         videoRef.current.currentTime = 0;
+//     };
+
+//     return (
+//         <div
+//             onMouseOver={handleMouseOver}
+//             onMouseOut={handleMouseOut}
+//         >
+//             <video ref={videoRef} style={{ width: '400px' }}>
+//                 <source src={src} type="video/mp4" />
+//                 Your browser does not support the video tag.
+//             </video>
+//         </div>
+//     );
+// };
+
+// export default VideoPlayer;
+
+import React from 'react'
+
+const VideoPlayer = () => {
+    return (
+        <div>VideoPlayer</div>
+    )
 }
 
-
-const VideoPlayer = ({ src }:Props) => {
-    const videoRef = useRef(null);
-
-    const handleMouseOver = () => {
-        videoRef.current.play();
-    };
-
-    const handleMouseOut = () => {
-        videoRef.current.pause();
-        videoRef.current.currentTime = 0;
-    };
-
-    return (
-        <div
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}
-        >
-            <video ref={videoRef} style={{ width: '400px' }}>
-                <source src={src} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-        </div>
-    );
-};
-
-export default VideoPlayer;
+export default VideoPlayer
 
 
